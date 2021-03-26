4f0624c8e8271f0c8592eea1b8d97e55a9a2c771
Pikmin1211 <joefarr1211@yahoo.ca>
2021-03-26 17:44:29 -0400

    multiple random unit spawns

--- Engine Hacks/FETD asm/Get_Enemies.asm
+++ Engine Hacks/FETD asm/Get_Enemies.asm
@@ -8,13 +8,30 @@ mov lr, \reg
 .short 0xF800
 .endm
 
+.equ MemorySlotC, 0x30004E8
+
 .equ NextRN_N, 0x08000c80
 
 push	{r14}
+
+mov r0, #0 // this will potentially get stored slot2
+
+// Enemy_Pointer_List_Tables offset
+
+ldr		r1,Enemy_Pointer_List_Tables
+ldr r2, = MemorySlotC
+ldr r2, [r2]
+mov r3, #4
+mul r2, r3
+add r1, r2
+ldr r1, [r1]
+cmp r1, #0
+beq end
+
 ldr		r0,=#0x202BCF0
 ldrh	r0,[r0,#0x10]		@turn number
 lsl		r0,#2
-ldr		r1,Enemy_Pointer_List_Table
+
 add		r0,r1
 ldr r0, [r0]
 cmp r0, #0
@@ -24,6 +41,7 @@ mov r4, r0 // r4 = list
 ldr r0, [r4] // # number of groups to pick from
 cmp r0, #0
 beq end
+
 add r4, #4 // go to the enemy groups
 blh NextRN_N
 mov r1, #4
@@ -37,5 +55,5 @@ pop		{r0}
 bx		r0
 
 .ltorg
-Enemy_Pointer_List_Table:
+Enemy_Pointer_List_Tables:
 @
